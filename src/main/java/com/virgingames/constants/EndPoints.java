696f8aec06cb5796369e8dad9f1e9f492ffbd9ac
chaitanyamehta1 <mehtachaitanya1179@gmail.com>
2020-06-04 17:55:04 +0100

    virgingames-backend

---
+++ src/main/java/com/virgingames/constants/EndPoints.java
@@ -0,0 +1,12 @@
+package com.virgingames.constants;
+
+/**
+ * Created By Chaitanya
+ */
+public class EndPoints {
+
+    // EndPoints defined for the Bingo lobby Feed
+
+    public static final String GET_BINGO_LOBBY_FEED = "/GetBingoLobbyFeed.do";
+
+}
